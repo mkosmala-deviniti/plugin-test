68f83de4a1b8927b3052ab149d5880f4cfa36dda
GitHub Action <action@github.com>
2021-04-11 11:33:59 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mkosmaladeviniti/plugintest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mkosmaladeviniti.plugintest.services
+
+import com.github.mkosmaladeviniti.plugintest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/mkosmaladeviniti/plugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mkosmaladeviniti/plugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mkosmaladeviniti.plugintest.services
 
+import com.github.mkosmaladeviniti.plugintest.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
